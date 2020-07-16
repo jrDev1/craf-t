8ef4202d9065e90b885bb8c70842203b1776cd4a
JrDevArts <eturgott.jrdevarts@gmail.com>
2020-07-15 22:28:21 -0400

    Update hnl.mobileConsole.js

--- hnl.mobileConsole.js
+++ hnl.mobileConsole.js
@@ -223,69 +223,69 @@
 // var mobileConsole = (function () {
 //   'use strict';
 
-//   //options and other variable containers
-//   var options = {
-//     overrideAutorun: false, //set this to true to skip mobile-detection and run the console no matter what.
-//     startMinimized: true,
-//     version: '1.3.5',
-//     baseClass: 'mobileConsole_',
-//     animParams: 'all 200ms ease',
-//     browserinfo: {
-//       isMobile: (function (a) {
-//         return (/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|ipad|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a) || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0, 4)));
-//       }(navigator.userAgent || navigator.vendor || window.opera)),
-//       browserChrome: /chrome/.test(navigator.userAgent.toLowerCase()),
-//       ffox: /firefox/.test(navigator.userAgent.toLowerCase()) && !/chrome/.test(navigator.userAgent.toLowerCase()),
-//       safari: /safari/.test(navigator.userAgent.toLowerCase()) && !/chrome/.test(navigator.userAgent.toLowerCase()),
-//       trident: /trident/.test(navigator.userAgent.toLowerCase()),
-//       evtLstn: typeof window.addEventListener === 'function'
-//     },
-//     methods: ['log', 'trace', 'info', 'warn', 'error', 'group', 'groupCollapsed', 'groupEnd', 'table', 'assert', 'time', 'timeEnd', 'clear'],
-//     hideButtons: ['group', 'groupCollapsed', 'groupEnd', 'table', 'assert', 'time', 'timeEnd'],
-//     ratio: 0.4, //screen/console-ratio that determines the height of the console (reevaluated on every minimize/maximize).
-//     paddingLeft: 0, //used when grouping, no need to change as it will be reset.
-//     groupDepth: 0, //used when grouping, no need to change as it will be reset.
-//     truncate: 400 //hard limit for large strings. For speed/mem issues with consecutive logging of large strings
-//   },
-//   messages = {
-//     clear : 'Console was cleared',
-//     empty: '(Empty string)'
-//   },
-//   status = {
-//     initialized: false,
-//     acActive : false,
-//     acHovered : false,
-//     acInput : '',
-//     timers : {}
-//   },
-//   history = {
-//     output : {
-//       prevMsg : '',
-//       prevMethod : '',
-//       counter : 0
-//     },
-//     input : {
-//       commands : window.sessionStorage ? (sessionStorage.getItem('mobileConsoleCommandHistory') ? JSON.parse(sessionStorage.getItem('mobileConsoleCommandHistory')) : []) : [],
-//       commandIdx: window.sessionStorage ? (sessionStorage.getItem('mobileConsoleCommandHistory') ? JSON.parse(sessionStorage.getItem('mobileConsoleCommandHistory')).length : 0) : 0,
-//       acIdx: 0,
-//       acHovered: false
-//     }
-//   },
-//   //'backup' original console for reference & internal debugging
-//   missingMethod = function() { return true; },  //method is not supported on this device's original console, return dummy
-//   originalConsole = {
-//     log:        (console && typeof console.log === 'function') ?       console.log.bind(console) :       missingMethod,
-//     info:       (console && typeof console.info === 'function') ?      console.info.bind(console) :      missingMethod,
-//     dir:        (console && typeof console.dir === 'function') ?       console.dir.bind(console) :       missingMethod,
-//     group:      (console && typeof console.group === 'function') ?     console.group.bind(console) :     missingMethod,
-//     groupEnd:   (console && typeof console.groupEnd === 'function') ?  console.groupEnd.bind(console) :  missingMethod,
-//     warn:       (console && typeof console.warn === 'function') ?      console.warn.bind(console) :      missingMethod,
-//     error:      (console && typeof console.error === 'function') ?     console.error.bind(console) :     missingMethod,
-//     trace:      (console && typeof console.trace === 'function') ?     console.trace.bind(console) :     missingMethod,
-//     clear:      (console && typeof console.clear === 'function') ?     console.clear.bind(console) :     missingMethod
-//   },
-//   // reference variables
-//   mobileConsole, consoleElement, commandLine;
+  //options and other variable containers
+  var options = {
+    overrideAutorun: false, //set this to true to skip mobile-detection and run the console no matter what.
+    startMinimized: true,
+    version: '1.3.5',
+    baseClass: 'mobileConsole_',
+    animParams: 'all 200ms ease',
+    browserinfo: {
+      isMobile: (function (a) {
+        return (/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|ipad|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a) || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0, 4)));
+      }(navigator.userAgent || navigator.vendor || window.opera)),
+      browserChrome: /chrome/.test(navigator.userAgent.toLowerCase()),
+      ffox: /firefox/.test(navigator.userAgent.toLowerCase()) && !/chrome/.test(navigator.userAgent.toLowerCase()),
+      safari: /safari/.test(navigator.userAgent.toLowerCase()) && !/chrome/.test(navigator.userAgent.toLowerCase()),
+      trident: /trident/.test(navigator.userAgent.toLowerCase()),
+      evtLstn: typeof window.addEventListener === 'function'
+    },
+    methods: ['log', 'trace', 'info', 'warn', 'error', 'group', 'groupCollapsed', 'groupEnd', 'table', 'assert', 'time', 'timeEnd', 'clear'],
+    hideButtons: ['group', 'groupCollapsed', 'groupEnd', 'table', 'assert', 'time', 'timeEnd'],
+    ratio: 0.4, //screen/console-ratio that determines the height of the console (reevaluated on every minimize/maximize).
+    paddingLeft: 0, //used when grouping, no need to change as it will be reset.
+    groupDepth: 0, //used when grouping, no need to change as it will be reset.
+    truncate: 400 //hard limit for large strings. For speed/mem issues with consecutive logging of large strings
+  },
+  messages = {
+    clear : 'Console was cleared',
+    empty: '(Empty string)'
+  },
+  status = {
+    initialized: false,
+    acActive : false,
+    acHovered : false,
+    acInput : '',
+    timers : {}
+  },
+  history = {
+    output : {
+      prevMsg : '',
+      prevMethod : '',
+      counter : 0
+    },
+    input : {
+      commands : window.sessionStorage ? (sessionStorage.getItem('mobileConsoleCommandHistory') ? JSON.parse(sessionStorage.getItem('mobileConsoleCommandHistory')) : []) : [],
+      commandIdx: window.sessionStorage ? (sessionStorage.getItem('mobileConsoleCommandHistory') ? JSON.parse(sessionStorage.getItem('mobileConsoleCommandHistory')).length : 0) : 0,
+      acIdx: 0,
+      acHovered: false
+    }
+  },
+  //'backup' original console for reference & internal debugging
+  missingMethod = function() { return true; },  //method is not supported on this device's original console, return dummy
+  originalConsole = {
+    log:        (console && typeof console.log === 'function') ?       console.log.bind(console) :       missingMethod,
+    info:       (console && typeof console.info === 'function') ?      console.info.bind(console) :      missingMethod,
+    dir:        (console && typeof console.dir === 'function') ?       console.dir.bind(console) :       missingMethod,
+    group:      (console && typeof console.group === 'function') ?     console.group.bind(console) :     missingMethod,
+    groupEnd:   (console && typeof console.groupEnd === 'function') ?  console.groupEnd.bind(console) :  missingMethod,
+    warn:       (console && typeof console.warn === 'function') ?      console.warn.bind(console) :      missingMethod,
+    error:      (console && typeof console.error === 'function') ?     console.error.bind(console) :     missingMethod,
+    trace:      (console && typeof console.trace === 'function') ?     console.trace.bind(console) :     missingMethod,
+    clear:      (console && typeof console.clear === 'function') ?     console.clear.bind(console) :     missingMethod
+  },
+  // reference variables
+  mobileConsole, consoleElement, commandLine;
 
   //helpers for all sub functions
   function setCSS(el, css) {
