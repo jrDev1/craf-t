4e40805ac05f45077c7c5c4e8fe281a49eba17dc
JrDevArts <eturgott.jrdevarts@gmail.com>
2020-08-04 11:26:52 -0400

    Update generalProcess.js

--- base/generalProcess.js
+++ base/generalProcess.js
@@ -1,7 +1,7 @@
 // 1. Get a target element that you want to persist scrolling for (such as a modal/lightbox/flyout/nav).
 // Specifically, the target element is the one we would like to allow scroll on (NOT a parent of that element).
 // This is also the element to apply the CSS '-webkit-overflow-scrolling: touch;' if desired.
-const targetElement = document.querySelector("#Main");
+const targetElement = document.getElementById("Main");
 
 // 2. ...in some event handler after showing the target element...disable body scroll
 bodyScrollLock.disableBodyScroll(targetElement);
