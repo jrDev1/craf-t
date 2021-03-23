3aa1d33039c7d6e858edc1208ee15ffcc3479f80
JrDevArts <eturgott.jrdevarts@gmail.com>
2021-03-23 13:58:49 -0400

    Update and rename threejsOutlineTest.js to Outline.js

---
+++ Outline.js
@@ -0,0 +1,44 @@
+  
+AFRAME.registerComponent('outline', {
+        
+        schema: {
+             
+                thickness: {type:'number', default: 0.003},
+                color: {type:'color', default: 'black'}
+  
+        },
+        
+        init: function () {
+          
+                // Wait for model to load.
+                this.el.addEventListener('model-loaded', () => {
+            
+                        console.log("MODEL LOADED");
+            
+                        // Grab the mesh / scene.
+                        const obj = this.el.getObject3D('mesh');
+            
+                        // Go over the submeshes and modify materials we want.
+                        obj.traverse(node => {
+              
+                                if (node.name.indexOf('great_white_shark_exp19') !== -1) {
+                
+                                        const s = obj.scale * this.data.thickness;
+
+                                        obj.scale.set(s,s,s);
+                        
+                                        node.material.color.set(this.data.color);
+                
+                                        node.material.side = THREE.BackSide
+                
+              
+                                }
+            
+                        });
+
+           
+                });
+         
+        }
+      
+});
