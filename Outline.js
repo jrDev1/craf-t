19357694b88d8404b7fc71bcd117b6921f6bdbe4
JrDevArts <eturgott.jrdevarts@gmail.com>
2021-03-23 14:10:05 -0400

    Update Outline.js

--- Outline.js
+++ Outline.js
@@ -25,7 +25,7 @@ AFRAME.registerComponent('outline', {
 
                                         obj.scale.set(s,s,s);
                         
-                        console.log("MODEL LOADED " + obj.scale);
+                        console.log("MODEL LOADED " + obj);
             
                                         node.material.color.set(this.data.color);
                 
