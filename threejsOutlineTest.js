3566cd4ba9da1d39eae87597d49efc45d299ed2a
JrDevArts <eturgott.jrdevarts@gmail.com>
2021-03-23 01:57:36 -0400

    Update threejsOutlineTest.js

--- threejsOutlineTest.js
+++ threejsOutlineTest.js
@@ -1,30 +1,34 @@
-//create a cube
-const mat = new THREE.MeshLambertMaterial({
-    color:’black’, 
-    side:THREE.BackSide 
-})
-mat.onBeforeCompile = (shader) => {
-    const token = ‘#include <begin_vertex>’
-    const customTransform = `
-        vec3 transformed = position + objectNormal*0.02;
-    `
-    shader.vertexShader = 
-        shader.vertexShader.replace(token,customTransform)
-}
 
-obj = new THREE.Group()
-const c1 = new THREE.Mesh(
-    new THREE.TorusKnotBufferGeometry(0.6,0.1),
-    mat
-)
-const s = 1.03
-c1.scale.set(s,s,s)
-obj.add(c1)
-obj.add(new THREE.Mesh(
-   new THREE.TorusKnotBufferGeometry(0.6,0.1),
-   new THREE.MeshPhongMaterial({
-       color:’yellow’, 
-       side: THREE.FrontSide
-   })
-))
+AFRAME.registerComponent('modify-materials', {
+        init: function () {
+          // Wait for model to load.
+          this.el.addEventListener('model-loaded', () => {
+            console.log("MODEL LOADED");
+            
+            //create a cube
+            const mat = new THREE.MeshLambertMaterial({
+                color:’black’, 
+                side:THREE.BackSide 
+            })
+            mat.onBeforeCompile = (shader) => {
+                const token = ‘#include <begin_vertex>’
+                const customTransform = `
+                    vec3 transformed = position + objectNormal*0.02;
+                `
+                shader.vertexShader = 
+                    shader.vertexShader.replace(token,customTransform)
+            }
 
+            // Grab the mesh / scene.
+            const obj = this.el.getObject3D('mesh');
+            
+            // Go over the submeshes and modify materials we want.
+            obj.traverse(node => {
+              if (node.name.indexOf('great_white_shark_exp19') !== -1) {
+                node.material= mat
+              }
+            });
+
+          });
+        }
+      });
